2cc3b997e8d04d79709b072a5b622dc3145d0c63
2395959141 <2395959141@qq.com>
2025-06-05 20:53:48 +0800

    feat：添加Data-jucier的启动脚本

--- bitbrain/data_process/step2/run.sh
+++ bitbrain/data_process/step2/run.sh
@@ -2,7 +2,8 @@
 #! 参考自项目：Steel-LLM
 #! 具体代码：https://github.com/zhanshijinwat/Steel-LLM/blob/main/data/pretrain_data_prepare/step2/run_step2.sh
 DJ_PATH=/home/chenyuhang/data-juicer/
-TXT_YAML=txt_process.yaml
+#TXT_YAML=zh_text_process.yaml   #* 中文文件的过滤
+TXT_YAML=en_text_process.yaml   #* 英文文件的过滤
 
 #python $DJ_PATH/tools/process_data.py --config $CODE_YAML 
 python $DJ_PATH/tools/process_data.py --config $TXT_YAML 
